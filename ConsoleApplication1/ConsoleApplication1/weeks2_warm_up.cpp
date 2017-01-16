0dbaea0d481486b0e7d4648d0b0b3da1fa602fcd
김경년 <knkknk1224@naver.com>
2017-01-16 19:24:12 +0900

    2주차 웜업 문제

---
+++ ConsoleApplication1/ConsoleApplication1/weeks2_warm_up.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	int firstNum, secondNum;
+	string first, second;
+	char fir[100000], sec[100000];
+	cin >> first >> second;
+	firstNum = first.size();
+	secondNum = second.size();
+
+	for (int i = 0; i < firstNum; i++)
+		fir[i] = first.at(i);
+	for (int j = 0; j < secondNum; j++)
+		sec[j] = second.at(j);
+
+	for (int a = 0; a < firstNum; a++)
+		first.at(a) = fir[(firstNum - a - 1)];
+	for (int b = 0; b < secondNum; b++)
+		second.at(b) = sec[(secondNum - b - 1)];
+	
+	int firChange = stoi(first,0,10);
+	int secChange = stoi(second,0, 10);
+
+	if (firChange - secChange > 0)
+		cout << firChange << endl;
+	else if (firChange - secChange <= 0)
+		cout << secChange << endl;
+}
